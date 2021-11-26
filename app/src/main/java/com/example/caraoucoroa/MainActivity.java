39d321bbb3c1fea6e1eb3b7069abb82d66a7b878
Elexandro Torres <elexandro.torres@gmail.com>
2021-11-26 18:57:43 -0300

    main activity funcionality complete

--- app/src/main/java/com/example/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton ibJogar;
+    private int valueLimit = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ibJogar = findViewById(R.id.ibJogar);
+
+        ibJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+
+                intent.putExtra("result", coinResult());
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private int coinResult() {
+        Random randomValue = new Random();
+
+        return randomValue.nextInt(valueLimit);
     }
 }
