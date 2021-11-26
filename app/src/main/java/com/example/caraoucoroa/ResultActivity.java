561252157d0e2b4cd265b4589fd161b3644cafe3
Elexandro Torres <elexandro.torres@gmail.com>
2021-11-26 19:07:57 -0300

    cara and coroa funcion implemented

--- app/src/main/java/com/example/caraoucoroa/ResultActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultActivity.java
@@ -2,13 +2,53 @@ package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView tvResult;
+    private ImageView ivResult;
+    private ImageButton ibVoltar;
+
+    private int coinResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        tvResult = findViewById(R.id.tvResult);
+        ivResult = findViewById(R.id.ivResult);
+        ibVoltar = findViewById(R.id.ibVoltar);
+
+        Intent intent = getIntent();
+        coinResult = intent.getExtras().getInt("result");
+
+        showResult();
+
+        ibVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void showResult() {
+        //Considerarei zero cara e 1 coroa.
+        if(coinResult == 0) {
+            ivResult.setImageResource(R.drawable.moeda_cara);
+            tvResult.setText("CARA");
+        }
+        else {
+            ivResult.setImageResource(R.drawable.moeda_coroa);
+            tvResult.setText("COROA");
+        }
     }
 }
